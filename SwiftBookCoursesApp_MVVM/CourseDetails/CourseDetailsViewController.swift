e2ec3f217525aeac001dad0f4bfeffbd1f62f8f8
auhustsinovich <avgustinovich4@gmail.com>
2022-08-15 15:56:54 +0200

    courseDetails completed

--- SwiftBookCoursesApp_MVVM/CourseDetails/CourseDetailsViewController.swift
+++ SwiftBookCoursesApp_MVVM/CourseDetails/CourseDetailsViewController.swift
@@ -9,21 +9,37 @@ import UIKit
 
 class CourseDetailsViewController: UIViewController {
 
+    @IBOutlet var courseNameLabel: UILabel!
+    @IBOutlet var numberOfLessonsLabel: UILabel!
+    @IBOutlet var numberOfTestsLabel: UILabel!
+    @IBOutlet var courseImage: UIImageView!
+    @IBOutlet var favoriteButton: UIButton!
+    
+    var courseDetailsViewModel: CourseDetailsViewModelProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func toggleFavourite(_ sender: UIButton) {
+        courseDetailsViewModel.favoriteButtonPressed()
     }
-    */
-
+    private func setupUI() {
+        setStatusForFavoriteButton(courseDetailsViewModel.isFavorite)
+        
+        courseDetailsViewModel.viewModelDidChange = { [weak self] viewModel in
+            self?.setStatusForFavoriteButton(viewModel.isFavorite)
+        }
+        
+        courseNameLabel.text = courseDetailsViewModel.courseName
+        numberOfLessonsLabel.text = courseDetailsViewModel.numberOfLessons
+        numberOfTestsLabel.text = courseDetailsViewModel.numberOfTests
+        courseImage.image = UIImage(data: courseDetailsViewModel.imageData ?? Data())
+    }
+    
+    private func setStatusForFavoriteButton(_ status: Bool) {
+        favoriteButton.tintColor = status ? .red : .gray
+    }
 }
+
